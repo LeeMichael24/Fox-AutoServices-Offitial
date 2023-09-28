ed92d5c63a249463f69e00e8a5dac34b5e737960
LeeMichael24 <leemichaeln24@gmail.com>
2023-09-27 23:53:40 -0600

    addinf some features

--- theme/js/scripts.js
+++ theme/js/scripts.js
@@ -88,6 +88,20 @@ sr.reveal(".animate-bottom", {
 
 
 
+  // Testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ]
+});
+
 
   //hero end services tabs
 
