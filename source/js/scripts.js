491da33b50acf342215a845587f5c8997817af8d
LeeMichael24 <leemichaeln24@gmail.com>
2023-09-30 03:52:34 -0600

    adding testimonials

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,110 +3,90 @@
 
 //Open menu on click
 selectElement(".open").addEventListener("click", () => {
-    selectElement(".nav-list").classList.add("active");
-})
+  selectElement(".nav-list").classList.add("active");
+});
 //Close menu on click
 selectElement(".close").addEventListener("click", () => {
-    selectElement(".nav-list").classList.remove("active");
-})
+  selectElement(".nav-list").classList.remove("active");
+});
 
 //Scroll reveal
 
 window.sr = new ScrollReveal();
 
 sr.reveal(".animate-left", {
-    origin: "left",
-    duration: 1100,
-    distance: "25rem",
-    delay: 60
+  origin: "left",
+  duration: 1100,
+  distance: "25rem",
+  delay: 60,
 });
 
 sr.reveal(".animate-right", {
-    origin: "right",
-    duration: 1100,
-    distance: "25rem",
-    delay: 60
+  origin: "right",
+  duration: 1100,
+  distance: "25rem",
+  delay: 60,
 });
 
 sr.reveal(".animate-top", {
-    origin: "top",
-    duration: 1000,
-    distance: "8rem",
-    delay: 40
+  origin: "top",
+  duration: 1000,
+  distance: "8rem",
+  delay: 40,
 });
 
 sr.reveal(".animate-bottom", {
-    origin: "bottom",
-    duration: 1000,
-    distance: "10rem",
-    delay: 45
+  origin: "bottom",
+  duration: 1000,
+  distance: "10rem",
+  delay: 45,
 });
-
 
 //How we do it rotate function
 (function ($) {
-	"use strict";
+  "use strict";
 
-	// navbarDropdown
-	if ($(window).width() < 992) {
-		$(".navigation .dropdown-toggle").on("click", function () {
-			$(this).siblings(".dropdown-menu").animate({
-				height: "toggle"
-			}, 300);
-		});
+  // navbarDropdown
+  if ($(window).width() < 992) {
+    $(".navigation .dropdown-toggle").on("click", function () {
+      $(this).siblings(".dropdown-menu").animate(
+        {
+          height: "toggle",
+        },
+        300
+      );
+    });
   }
 
-
   $(window).on("scroll", function () {
-		counter();
-	});
+    counter();
+  });
 
+  // Shuffle js filter and masonry
+  if ($(".shuffle-wrapper").length !== 0) {
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
 
-	// Shuffle js filter and masonry
-	if ($(".shuffle-wrapper").length !== 0) {
-		var Shuffle = window.Shuffle;
-		var jQuery = window.jQuery;
-
-		var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
-			itemSelector: ".shuffle-item",
-			buffer: 1
-		});
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
-
-
-
-  // Testimonial carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    items: 1,
-    loop: true,
-    dots: false,
-    nav: true,
-    navText : [
-        '<i class="bi bi-arrow-left"></i>',
-        '<i class="bi bi-arrow-right"></i>'
-    ]
-});
-
+    var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
+      itemSelector: ".shuffle-item",
+      buffer: 1,
+    });
+    jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
+      var input = evt.currentTarget;
+      if (input.checked) {
+        myShuffle.filter(input.value);
+      }
+    });
+  }
 
   //hero end services tabs
-
 })(jQuery);
-
-
 
 let customSlideIndex = 1;
 showCustomSlide(customSlideIndex);
 
 function changeCustomSlide(n) {
-  showCustomSlide(customSlideIndex += n);
+  showCustomSlide((customSlideIndex += n));
 }
 
 function showCustomSlide(n) {
@@ -123,9 +103,7 @@ function showCustomSlide(n) {
   customSlides[customSlideIndex - 1].style.display = "block";
 }
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var video = document.querySelector(".hero-video-commercial");
 
   // Agrega la ruta local del video al src
@@ -134,12 +112,11 @@ document.addEventListener("DOMContentLoaded", function() {
   video.src = "images/video/videoOficial.webm"; // Cambia la ruta a tu video local
 
   // Escuchar el evento "canplaythrough" para saber cuando el video está completamente cargado
-  video.addEventListener("canplaythrough", function() {
-      // Comienza a reproducir el video automáticamente
-      video.play();
+  video.addEventListener("canplaythrough", function () {
+    // Comienza a reproducir el video automáticamente
+    video.play();
   });
 });
-
 
 //Escribir automatico en home palabra "Experience"
 const typed = new Typed(".typed", {
@@ -150,3 +127,8 @@ const typed = new Typed(".typed", {
   backDelay: 2000,
   loop: true,
 });
+
+/* GLide section functions */
+
+
+
